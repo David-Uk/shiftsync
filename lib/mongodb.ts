90f73579918b7a87da8312fe30d668b6b0d1adb3
David Ukelere <david.ukelere291@gmail.com>
2026-03-01 06:35:06 +0100

    Prepped production build

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL!;
+constMONGO_URL = process.env.MONGODB_URL!;
 
 if (!MONGODB_URL) {
   throw new Error(
-    "Please define the MONGODB_URL environment variable inside .env",
+    "Please define theMONGO_URL environment variable inside .env",
   );
 }
 
